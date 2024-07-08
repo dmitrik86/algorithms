9fec2a6b09a4f1e1d947ea0037279ae8ba6d4092
Dmytro Mashkin <dmitriy.mashkin86@gmail.com>
2024-07-08 19:25:48 +0300

    Remove Duplicates From an Unsorted Linked List Solution. Python

---
+++ remove-duplicates-from-an-unsorted-linked-list/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
+        counter = defaultdict(int)
+        node = head
+        while node:
+            counter[node.val] += 1
+            node = node.next
+        while head:
+            if counter[head.val] == 1:
+                break
+            head = head.next
+        if not head:
+            return None
+        node = prev = head
+        while node:
+            if counter[node.val] == 1:
+                if prev != node:
+                    prev.next = node
+                prev = node
+            node = node.next
+        prev.next = None
+        return head
